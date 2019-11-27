5c1dbbd8d9505957fd27268b5883d538b0422c98
ajay1297 <ajayppatil100@gmail.com>
2019-11-27 23:04:44 +0530

    Updates

---
+++ precache-manifest.3cc953fa2d0ba7c9cd2ee1ebb3b603f6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "80edcfa822db2be4b63b889166b38a85",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "ecbdf234fdef9f406986",
+    "url": "/monsters-rolodex/static/css/main.b8bf74e1.chunk.css"
+  },
+  {
+    "revision": "1ea6f39e1d0bc9ed7aad",
+    "url": "/monsters-rolodex/static/js/2.02c021cd.chunk.js"
+  },
+  {
+    "revision": "ecbdf234fdef9f406986",
+    "url": "/monsters-rolodex/static/js/main.b73bc277.chunk.js"
+  },
+  {
+    "revision": "a7cca9c0025350a4fceb",
+    "url": "/monsters-rolodex/static/js/runtime-main.0fb999f3.js"
+  }
+]);
